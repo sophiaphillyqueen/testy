77675e886381c27ee91ab96fb340f59a4f8d49d5
Sophia Elizabeth Shapira <Sophia.Shapira@gmail.com>
2014-08-27 12:03:24 -0400

    From now on, attempts to discern what the local C compiler is will be segregated to a special script.

---
+++ ident-local-cc.sh
@@ -0,0 +1,36 @@
+# ident-local-cc.sh: Identifies local C compiler
+# Copyright (C) 2014  Sophia Elizabeth Shapira
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# ########################
+
+# Of course, the current version of this program simply
+# ASSumes that "gcc" is used as the local compiler for
+# binaries that will run on this machine, but the idea
+# is that it will later be changed to no longer make this
+# ASSumption. Granted, this script will only be needed
+# for installation of the *base* -chorebox- package.
+# For later versions, the -chorebox- configuration utility
+# will do this calculation if need-be.
+
+# This program is intended for the calling shell-script
+# to call with *all* of it's command-line arguments - the
+# idea being that this script will have all the same
+# information that it's calling script has access to to
+# make the necessary determination.
+
+echo gcc
+
+
